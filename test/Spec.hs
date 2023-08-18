1a04aa389ba576b44aa05794e0bf38e764b1eb80
Joel McCracken <mccracken.joel@gmail.com>
2023-08-18 17:35:27 -0400

    wer got to parseLinesState

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,25 +2,44 @@
 
 import Test.Hspec
 
-import qualified Text.Parsec  as Parsec
+import qualified Text.Parsec as Parsec
 import Text.Parsec hiding (parse)
 
-import Control.Applicative
+import Control.Applicative (pure)
 
 import Control.Monad.Identity (Identity)
-
+import Control.Monad
 import Data.List
 
 parse :: Parsec String () a -> String -> Either ParseError a
 parse rule text = Parsec.parse rule "(source)" text
 
+parseLinesState :: String -> Either ParseError Integer
+parseLinesState input = do
+  let
+    matchLine = do
+      void $ manyTill anyChar (try (string "\n"))
+      modifyState (+1)
+      lineParser
+    lineParser :: Parsec String Integer ()
+    lineParser =
+      (void $ try (manyTill (noneOf "\n") (try eof) )) <|> matchLine
+  runParser (lineParser >> getState) 1 "filename.hs" input
+
+parseLines :: String -> Either ParseError Integer
+parseLines input = do
+  let
+    lineParser i =
+      try (manyTill (noneOf "\n") (try eof) >> pure i) <|>
+         (manyTill anyChar (try (string "\n")) >> lineParser (i+1))
+  parse (lineParser 1) input
+
 parseStatic :: String -> Either ParseError Integer
 parseStatic input = do
   let
     rules = do
-      manyTill anyChar (try (string "\n"))
-      manyTill anyChar (try (string "\n"))
-      manyTill anyChar eof
+      void $ manyTill anyChar (try (string "\n"))
+      -- void $ manyTill anyChar eof
       pure 10
   parse rules input
 
@@ -33,3 +52,11 @@ main = hspec $ do
                   , "c"
                   ]
     parseStatic input `shouldBe` Right 10
+    parseLines input `shouldBe` Right 3
+    let input2 = intercalate "\n"
+                  [ "a"
+                  , "b"
+                  , "c"
+                  , "d"
+                  ]
+    parseLinesState input2 `shouldBe` Right 4
