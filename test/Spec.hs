c1f2978365de948fbc519ff5aa49f13010849a5a
Joel McCracken <mccracken.joel@gmail.com>
2023-08-18 16:44:45 -0400

    parseStatic

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,10 +9,27 @@ import Control.Applicative
 
 import Control.Monad.Identity (Identity)
 
+import Data.List
+
 parse :: Parsec String () a -> String -> Either ParseError a
 parse rule text = Parsec.parse rule "(source)" text
+
+parseStatic :: String -> Either ParseError Integer
+parseStatic input = do
+  let
+    rules = do
+      manyTill anyChar (try (string "\n"))
+      manyTill anyChar (try (string "\n"))
+      manyTill anyChar eof
+      pure 10
+  parse rules input
 
 main :: IO ()
 main = hspec $ do
   it "whatever" $ do
-    'a' `shouldBe` 'b'
+    let input = intercalate "\n"
+                  [ "a"
+                  , "b"
+                  , "c"
+                  ]
+    parseStatic input `shouldBe` Right 10
