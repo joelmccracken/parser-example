286f17bba616e1c97f012f024df323b99d3d6fae
Joel McCracken <mccracken.joel@gmail.com>
2023-08-18 16:32:40 -0400

    initial setup

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+
+
+import Test.Hspec
+
+import qualified Text.Parsec  as Parsec
+import Text.Parsec hiding (parse)
+
+import Control.Applicative
+
+import Control.Monad.Identity (Identity)
+
+parse :: Parsec String () a -> String -> Either ParseError a
+parse rule text = Parsec.parse rule "(source)" text
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  it "whatever" $ do
+    'a' `shouldBe` 'b'
